b31ee3994e527d4d39a5ff954de695fd51557032
Moise Uwimana <moiseuwimana04@gmail.com>
2023-09-25 19:21:38 +0200

    Pass props to card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
-function Card() {
+function Card(props) {
   return (
     <div className="card">
-      <img className="card--image" src={require("../images/katie-zaferes.png")} />
+      <img className="card--image" src={require(`../images/${props.img}`)} />
       <div className="card--stats">
         <img className="card--star" src={require("../images/star.png")} />
-        <span>5.0</span>
-        <span className="gray">(6) &#183;</span>
-        <span className="gray">USA</span>
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) &#183;</span>
+        <span className="gray">{props.country}</span>
       </div>
-      <p>Life Lessons with Katie Zaferes</p>
-      <p><span className="bold">From $136</span> / person</p>
+      <p>{props.title}</p>
+      <p><span className="bold">From ${props.price}</span> / person</p>
       
     </div>
   )
