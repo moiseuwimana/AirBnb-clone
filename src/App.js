c24b9722c3af6d0063cd8323297aa3d642ef9ac6
Moise Uwimana <moiseuwimana04@gmail.com>
2023-09-26 00:05:45 +0200

    Map experience data into components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import NavBar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import data from './data'
+
+
+/*
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar (in place of the current <Card /> component)
+
+Note: We haven't syled the group of components yet, so they'll still be block elements, stacked vertically. we'll add styling later.
+*/
+
+
 
 
 export default function App() {
+  const cardElements = data.map((card)=>{
+    return (
+      <Card 
+      img = {card.coverImg}
+      rating = {card.stats.rating}
+      reviewCount = {card.stats.reviewCount}
+      country = {card.location}
+      title = {card.title}
+      price = {card.price}
+    />
+    )
+  })
   return (
     <div>
       <NavBar />
       {/* <Hero /> */}
-      <Card 
-         img = "katie-zaferes.png"
-         rating = "5.0"
-         reviewCount = {6}
-         country = "USA"
-         title = "Life Lessons with Katie Zaferes"
-         price = {136}
-      />
+      {cardElements}
     </div>
 
   )
